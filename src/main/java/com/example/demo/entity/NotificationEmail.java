ff1849185307d69c0a0ce7b30ee09d0ebc17180c
ShabdanovIlim <sabdanovilim@gmail.com>
2023-12-15 18:04:43 +0600

    add authentication and authorization methods

--- src/main/java/com/example/demo/entity/NotificationEmail.java
+++ src/main/java/com/example/demo/entity/NotificationEmail.java
@@ -1,4 +1,18 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "notification-email")
+@Entity
 public class NotificationEmail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "email_id_gen")
+    @SequenceGenerator(name = "email_id_gen", sequenceName = "email_id_seq", allocationSize = 1)
+    private Long id;
 }
--- src/main/java/com/example/demo/enums/VoteType.java
+++ src/main/java/com/example/demo/enums/VoteType.java
@@ -1,7 +1,7 @@
 package com.example.demo.enums;
 
 public enum VoteType {
-    UPVOTE(1),DOWNVOTE(-1)
+    UPVOTE(1),DOWNVOTE(-1);
     VoteType(int direction){
 
     }
